24354ac9b8f44d588d09016eebd77f9a648abdf1
harry_liou <harry_liou@asus.com>
2019-05-22 10:10:59 +0800

    panel: toshiba-tc358762 dsi panel porting for Tinker Edge R
    Porting panel on DSI-0 port for Tinker Edge R, and use vopl
    to show display
    
    Change-Id: Ie34205d34ae87475b7bf8a7923f0f1fb9c68d0a3

--- drivers/gpu/drm/rockchip/dw-mipi-dsi.c
+++ drivers/gpu/drm/rockchip/dw-mipi-dsi.c
@@ -866,7 +866,7 @@ static ssize_t dw_mipi_dsi_transfer(struct dw_mipi_dsi *dsi,
 		regmap_update_bits(dsi->regmap, DSI_LPCLK_CTRL,
 				   PHY_TXREQUESTCLKHS, 0);
 	} else {
-		regmap_update_bits(dsi->regmap, DSI_VID_MODE_CFG, LP_CMD_EN, 0);
+		// regmap_update_bits(dsi->regmap, DSI_VID_MODE_CFG, LP_CMD_EN, 0);
 		regmap_update_bits(dsi->regmap, DSI_LPCLK_CTRL,
 				   PHY_TXREQUESTCLKHS, PHY_TXREQUESTCLKHS);
 	}
@@ -1021,7 +1021,7 @@ static const struct mipi_dsi_host_ops dw_mipi_dsi_host_ops = {
 
 static void dw_mipi_dsi_video_mode_config(struct dw_mipi_dsi *dsi)
 {
-	u32 val = LP_VACT_EN | LP_VFP_EN | LP_VBP_EN | LP_VSA_EN |
+	u32 val = LP_VACT_EN | LP_VFP_EN | LP_VBP_EN | LP_VSA_EN | LP_CMD_EN |
 		  LP_HFP_EN | LP_HBP_EN;
 
 	if (dsi->mode_flags & MIPI_DSI_MODE_VIDEO_HFP)
@@ -1613,6 +1613,9 @@ encoder_cleanup:
 	return ret;
 }
 
+#if defined(CONFIG_TINKER_MCU)
+extern int tinker_mcu_is_connected(int dsi_id);
+#endif
 static int dw_mipi_dsi_bind(struct device *dev, struct device *master,
 			     void *data)
 {
@@ -1633,6 +1636,15 @@ static int dw_mipi_dsi_bind(struct device *dev, struct device *master,
 		if (!dsi->bridge)
 			return -EPROBE_DEFER;
 	}
+
+#if defined(CONFIG_TINKER_MCU)
+	if(!tinker_mcu_is_connected(dsi->id)) {
+		pr_info("dsi-%d panel isn't connected\n", dsi->id);
+		return 0;
+	} else {
+		pr_info("dsi-%d panel is connected\n", dsi->id);
+	}
+#endif
 
 	ret = dw_mipi_dsi_register(drm, dsi);
 	if (ret) {
